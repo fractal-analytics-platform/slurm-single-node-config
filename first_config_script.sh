0bffb634890f233001390f0169d94494a828aec1
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2025-06-26 15:24:58 +0200

    first_config_script.sh

--- first_config_script.sh
+++ first_config_script.sh
@@ -13,15 +13,23 @@ fi
 # Apt update
 sudo apt update -y
 sudo apt upgrade -y
+echo "--- end of apt update/upgrade ---"
+echo
 
 # SLURM configuration
 BASE_REPO_URL=https://raw.githubusercontent.com/fractal-analytics-platform/slurm-single-node-config/refs/heads/main
 echo "Fetch $BASE_REPO_URL/config/cgroup.conf"
-curl "$BASE_REPO_URL/config/cgroup.conf" -o /etc/slurm/cgroup.conf
+curl -q "$BASE_REPO_URL/config/cgroup.conf" -o /etc/slurm/cgroup.conf
+echo
+
 echo "Fetch $BASE_REPO_URL/config/slurmdbd.conf"
-curl "$BASE_REPO_URL/config/slurmdbd.conf" -o /etc/slurm/slurmdbd.conf
+curl -q "$BASE_REPO_URL/config/slurmdbd.conf" -o /etc/slurm/slurmdbd.conf
+echo
+
 echo "Fetch $BASE_REPO_URL/config/$LABEL.slurm.conf"
-curl "$BASE_REPO_URL/config/$LABEL.slurm.conf" -o /etc/slurm/slurm.conf
+curl -q "$BASE_REPO_URL/config/$LABEL.slurm.conf" -o /etc/slurm/slurm.conf
+echo
+
 chmod 600 /etc/slurmdbd.conf
 HOSTNAME=$(hostname)
 sed --in-place=".backup" -e "s/__REPLACE_HOSTNAME__/$HOSTNAME/g" /etc/slurm/slurm.conf
